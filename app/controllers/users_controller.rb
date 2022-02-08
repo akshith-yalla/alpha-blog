aee4b57a971ebc068c062ba092feb69c90a0605d
akshith-yalla <akshith.yalla@zemosolabs.com>
2022-02-08 16:20:05 +0530

    added index for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :get_user, only: [:show, :update, :edit, :destroy]
 
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
