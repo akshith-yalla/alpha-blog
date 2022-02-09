6d5d4beb0a4891a1acb2d7c598bbeed330c3c464
akshith-yalla <akshith.yalla@zemosolabs.com>
2022-02-09 16:08:02 +0530

    added category new form and contoller to create category and test it

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,9 +4,25 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
   end
 
   def show
+  end
+
+  def create
+    @category = Category.new(categeory_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def categeory_params
+    params.require(:category).permit(:name)
   end
 
 end
