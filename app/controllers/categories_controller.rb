ee83f2b9343fd5e33527994c9a740e348d5d2299
akshith-yalla <akshith.yalla@zemosolabs.com>
2022-02-09 18:49:26 +0530

    added edit in category and fixed routing in articles

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -16,7 +16,7 @@ class CategoriesController < ApplicationController
   end
 
   def create
-    @category = Category.new(categeory_params)
+    @category = Category.new(category_params)
     if @category.save
       flash[:notice] = "Category was successfully created"
       redirect_to @category
@@ -25,8 +25,22 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category was updated successfully"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
   private
-  def categeory_params
+  def category_params
     params.require(:category).permit(:name)
   end
 
