658d8515ffc6affb39b1cf8cf6a481664826b024
akshith-yalla <akshith.yalla@zemosolabs.com>
2022-02-09 16:33:05 +0530

    added integration test to check blank field validation

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -12,4 +12,15 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
       assert_response :success
       assert_match "Health", response.body
   end
+
+  test "get new catgeory form and reject invalid category submission" do
+    get new_category_url
+    assert_response :success
+    assert_no_difference 'Category.count' do
+      post categories_url, params: { category: { name: " " } }
+    end
+      assert_match "errors", response.body
+      assert_select 'div.alert'
+      assert_select 'h4'
+  end
 end
